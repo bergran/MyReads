db16a1a296429b6cf4b8d0c92f2d17467ea48135
bergran <bergraan@gmail.com>
2017-09-11 08:54:40 +0200

    refactor: Adding sort in search results

--- src/bookSearchResults.js
+++ src/bookSearchResults.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Book from './book'
+import sortBy from 'sort-by'
 
-const BookSearchResult = (props) => (
-  <div className="search-books-results">
-    <ol className="books-grid">
-      {
-        props.books.map((book) =>
-        <li key={book.id}>
-          <Book selectShelf={() => {}}
-            {...book}
-          />
-        </li>
-      )}
-    </ol>
-  </div>
-)
+const BookSearchResult = (props) => {
+    const bookSorted = props.books.sort(sortBy('title'))
+    return (
+    <div className="search-books-results">
+        <ol className="books-grid">
+            {
+                bookSorted.map((book) =>
+                    <li key={book.id}>
+                        <Book selectShelf={() => {}}
+                              {...book}
+                        />
+                    </li>
+                )}
+        </ol>
+    </div>
+    )
+}
 
 export { BookSearchResult }
