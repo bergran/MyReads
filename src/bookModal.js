5be59a46f93ec9e3782ee40da347dc33e5241775
bergran <bergraan@gmail.com>
2017-09-12 17:29:25 +0200

    feat: content book (not finished)

--- src/bookModal.js
+++ src/bookModal.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-
+import { Shelfs } from './shelfObject'
 const BookModal = props => {
   const { book } = props
+  const shelfObject = new Shelfs()
+  const auxShelf = book.shelf || 'none'
+  const shelf = shelfObject.getName(auxShelf)
+  let starts = []
+  for (let i = 0; i < 5; i++) {
+    if (i < book.averageRating) {
+      starts.push(<div className='rate star'></div>)
+    } else {
+      starts.push(<div className='rate nstar'></div>)
+    }
+  }
+
+
   return (
     <div className="modal">
       <div className="modalWindow">
@@ -19,6 +32,34 @@ const BookModal = props => {
             }}></div>
           <div className='modal-description'>
               {book.description}
+          </div>
+          <div className='modal-characters'>
+            <ul className='modal-properties'>
+              <li className='elementProperty'>
+                <p className='property'><strong>Publish date:</strong> {book.publishedDate}</p>
+              </li>
+              <li className='elementProperty'>
+                <p className='property'><strong>Shelf:</strong> { shelf }</p>
+              </li>
+              <li className='elementProperty'>
+                <p className='property'><strong>Shelf:</strong>
+                    {starts}
+                </p>
+              </li>
+              <li className='elementProperty'>
+                <strong>Authors:</strong>
+                <ul>
+                    {
+                        book.authors.map(
+                            author =>
+                                <li>
+                                    {author}
+                                </li>
+                        )
+                    }
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
